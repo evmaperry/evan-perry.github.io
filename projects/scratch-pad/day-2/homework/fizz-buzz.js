9190f17d39a2fd78da10b42042be1593d3356a91
Evan Perry <evmaperry@gmail.com>
2023-04-25 15:07:12 +0000

    fizzbuzz

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,7 +12,23 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
+    // declare for loop that iterates from 1 to 100
+    for (let i = 1; i <= 100; i++){
+        // declare conditional to check if i is divisible by 3 and 5
+        if (i%3===0 && i%5 === 0){
+            console.log("FizzBuzz"; // if so, log 'FizzBuzz' to console
+        }
+        // declare else if to check if i is divisible by 3
+        else if (i%3===0){
+            console.log("Fizz"); // if so, log 'Fizz' to console
+        }
+        // declare else if to check if i is divisible by 5
+        else if (i%5===0){
+            console.log("Buzz") // if so, log 'Buzz' to console
+        }
+        // declare else to otherwise print i
+        else {console.log(i);}
+    }
 
 
     // YOUR CODE GOES ABOVE HERE //
