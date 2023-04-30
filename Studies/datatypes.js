21306d01bdd13979aa613f1c284e20c1a802fe80
Evan Perry <evmaperry@gmail.com>
2023-04-30 15:52:42 +0000

    studies project

---
+++ Studies/datatypes.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * DATATYPES
+ * 
+ * 1) Number
+ * 
+ * 2) String
+ * 
+ * 3) Boolean
+ * 
+ * 4) Array
+ * 
+ * 5) Object
+ * 
+ * 6) Function
+ * 
+ * 7) undefined
+ * 
+ * 8) null
+ * 
+ * 9) NaN
+ * 
+ * 10) Infinity and -Infinity
+ * 
+ * 11) What is the difference between primitive/simple and complex datatypes
+ * 
+ * 12) Primitive values passed by copy versus complex values by reference.
+ *     What does that mean? How are they different?
+ * 
+ * 
+ */
---
+++ Studies/operators.js
@@ -0,0 +1,17 @@
+/**
+ * OPERATORS
+ * 
+ * 1) Assignment operators
+ * 
+ * 2) Arithmetic operators
+ * 
+ * 3) Comparison operators
+ * 
+ * 4) Logical operators
+ * 
+ * 5) Unary Operators (!, typeOf, -)
+ * 
+ * 6) Ternary operator (a ? b : c)
+ * 
+ * 
+ */
