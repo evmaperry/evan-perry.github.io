d1080f71b86acc0fb3a19301bca44cf2db015be8
Evan Perry <evmaperry@gmail.com>
2023-04-30 15:46:37 +0000

    worked on studies project

---
+++ Studies/studies-instruction-notes.js
@@ -0,0 +1,16 @@
+/** 
+ * EACH ENTRY SHOULD HAVE...
+ * 
+ * 1) An introduction (4-5 sentences)
+ * 
+ * 2) Clear explanation for each concept
+ * 
+ * 3) Working code examples for each concept
+ * 
+ * 4) Comment that exposits what each code example should do/return
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
--- Studies/variables.js
+++ Studies/variables.js
@@ -1,10 +1,23 @@
-// Topics included 
+ 
 
-// Exposition
 
-// introduction
-// 1. declaration and assignment
-// 2. var, let, const
-// 3. hoisting
-
-// Code
+/**
+ * VARIABLES
+ * 
+ * 
+ * 
+ * Introduction
+ * 
+ * 
+ * 1. Declaration and Assignment
+ * 
+ * 
+ * 
+ * 2. Var, Let, Const
+ * 
+ * 
+ * 3. Hoisting
+ * 
+ * 
+ * 
+ */
