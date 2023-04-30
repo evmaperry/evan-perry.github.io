b638c9e8f7f6dedbd19993027bc99450f10323bb
Evan Perry <evmaperry@gmail.com>
2023-04-30 16:02:54 +0000

    finished outlines for studies project

---
+++ Studies/functions.js
@@ -0,0 +1,25 @@
+/**
+ * FUNCTIONS
+ * 
+ * 1) Two phases of function construction: declaration and call/execute
+ * 
+ * 2) What's the difference between a function's parameters and arguments Passed
+ *    to a function
+ * 
+ * 3) What the syntax for a named function
+ * 
+ * 4) How do we assign a function to a variable
+ * 
+ * 5) Functions can optionally take inputs, and optionally return a single value.
+ *    How do we specify inputs, and how do we specifty outputs.
+ * 
+ * 6) Scope: functions can see and modify variables in parent or global scopes.
+ *    The inverse is NOT true.
+ * 
+ * 7) Closures: Functions form closures around the data they house.
+ *    If an object returned from the Function and is held in memory somewhere
+ *    (ie, referenced), that closure stays ALIVE, and data continue to exist
+ *    in these closures.
+ * 
+ * 
+ */
---
+++ Studies/loops.js
@@ -0,0 +1,17 @@
+/**
+ * LOOPS
+ * 
+ * 1) Explain while, for, and for-in loops
+ * 
+ * 
+ * 2) Be able to loop any number of times, forward counting up
+ *    to some limit, backward counting down to 0
+ * 
+ * 
+ * 3) Loop over an array, forwards and backwards
+ * 
+ * 
+ * 4) Loop over an Object
+ * 
+ * 
+ */
