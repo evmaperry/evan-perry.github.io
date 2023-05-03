49c8364ecc81591f7fd5975da36be70c3f85b3b5
Evan Perry <evmaperry@gmail.com>
2023-05-03 23:49:41 +0000

    working on eloquent JS chapter 2 exercises

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -3,23 +3,35 @@
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function triangles() {
-
+function triangles(num) { // declare triangles function in terms of char parameter
+  let printString = "" // declare printString variable to store and print chars
+  for (let i = 1 ; i <= num ; i++){ // declare for loop to iterate 7 times
+    printString += "#"; // concatenate '#' to printString each iteration
+    console.log(printString, "\n"); // log printString and newline to console each iteration
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function fizzBuzz() {
-  
+function fizzBuzz() { 
+  for (let i = 1; i <= 15 ; i++){ // declare for loop to iterate from 1 to 15
+    if (i % 5 === 0 && i % 3 === 0){ // declare conditional to evaluate if i is divisible by both 5 and 3
+      console.log("fizzbuzz"); // if so, log 'fizzbuzz'
+    } else if (i % 3 === 0){ // declare else-if to evaluate if i is divisible by 3
+      console.log("fizz"); // if so, log 'fizz'
+    } else if (i % 5 === 0){ // declare else-if to evaluate if i is divisible by 5
+      console.log("buzz"); // if so, log 'buzz'
+    } else {console.log(i);} // else log i
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard() { // needs a parameter
+  
 }
 
 ////////////////////////////////////////////////////////////////////////////////
